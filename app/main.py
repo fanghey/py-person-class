bf0dfc40c203100a7030e3d4f40cefb1b04c1b7f
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2025-06-01 17:22:23 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = [Person(data["name"], data["age"])
+              for data in people]
+
+    for per, human in enumerate(people):
+        full = {person.name: person for person in result}
+        if "wife" in human and human["wife"] is not None:
+            result[per].wife = full[human["wife"]]
+        elif "husband" in human and human["husband"] is not None:
+            result[per].husband = full[human["husband"]]
+
+    return result
