7b9a0410937cd1454908c33ab343b802415076ba
fanghey <2603Lysenko2603@gmail.com>
2026-01-30 11:55:37 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
 
+    def __repr__(self):
+        return f"Person(name='{self.name}', age={self.age})"
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    @staticmethod
+    def create_person_list(people: list) -> list:
+        person_list = []
+
+        for item in people:
+            if isinstance(item, tuple):
+                name, age = item
+                person = Person(name, age)
+                person_list.append(person)
+            elif isinstance(item, str):
+                person = Person(item, 0)
+                person_list.append(person)
+
+        return person_list
